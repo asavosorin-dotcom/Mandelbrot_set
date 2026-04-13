335f6ec781093514475aa4ecf96d8199f5261914
asavosorin-dotcom <asavosorin@gmail.com>
2026-04-13 13:55:04 +0300

    Версия для снятия показаний

--- version_24_intrin.cpp
+++ version_24_intrin.cpp
@@ -3,9 +3,15 @@
 #include <time.h>
 #include "raylib.h"
 #include <xmmintrin.h>
+#include <x86intrin.h>
+
+// #define DRAW
 
 int main()
 {
+    FILE* lab_file = fopen("laba.csv", "a");
+    fprintf(lab_file, "%s \n", __FILE__);
+
     const int screenWidth  = 1600;
     const int screenHeight = 900;
 
@@ -19,14 +25,13 @@ int main()
     __m128 N = {0}, 
            N_ITER = {0};
 
-    __m128 check_N = {0}; // после этой замены стало медленне на этапе замены массивов на векторы
     char check_n = 0;
 
     int ix = 0, 
         iy = 0;
     
-    float x_start = -1.2, 
-          y_start = -1.5;
+    float x_start = -2.3, 
+          y_start = -0.5;
 
     float dx = 0.001,
           dy = 0.001;
@@ -53,7 +58,9 @@ int main()
  
     __m128 R2 = {0},
            R2_CMP = {0}, 
-           R2MAX = _mm_set1_ps(100);
+           R2MAX = _mm_set1_ps(r2max);
+
+    int counter = 0;
 
     Texture2D My_texture = {};
     Image My_image = {};
@@ -65,21 +72,23 @@ int main()
     My_image.format = PIXELFORMAT_UNCOMPRESSED_R8G8B8A8;
 
     InitWindow(screenWidth, screenHeight, "hello");
-    SetTargetFPS(60);
         
     while (!WindowShouldClose())
     {
-        clock_t start = clock();
+        unsigned long long int start = __rdtsc();
+        ddx = dx / 10;
+        ddy = dy / 10;
 
         Y0 = _mm_set1_ps(y_start);
+    #ifdef DRAW
+        if (IsKeyDown(KEY_J)) y_start += 50 * dy;
+        if (IsKeyDown(KEY_L)) x_start += 50 * dx;
+        if (IsKeyDown(KEY_K)) y_start -= 50 * dy;
+        if (IsKeyDown(KEY_H)) x_start -= 50 * dx;
 
-        if (IsKeyPressedRepeat(KEY_J)) y_start += 50 * dy;
-        if (IsKeyPressedRepeat(KEY_L)) x_start += 50 * dx;
-        if (IsKeyPressedRepeat(KEY_K)) y_start -= 50 * dy;
-        if (IsKeyPressedRepeat(KEY_H)) x_start -= 50 * dx;
-
-        if (IsKeyPressedRepeat(KEY_Z)) dx += ddx, dy += ddy, x_start -= screenWidth / 2 * ddx, y_start -= screenHeight / 2 * ddy;
-        if (IsKeyPressedRepeat(KEY_X)) dx -= ddx, dy -= ddy, x_start += screenWidth / 2 * ddy, y_start += screenHeight / 2 * ddy;
+        if (IsKeyDown(KEY_Z)) dx += ddx, dy += ddy, x_start -= screenWidth / 2 * ddx, y_start -= screenHeight / 2 * ddy;
+        if (IsKeyDown(KEY_X)) dx -= ddx, dy -= ddy, x_start += screenWidth / 2 * ddy, y_start += screenHeight / 2 * ddy;
+    #endif
 
         for (iy = 0; iy < screenHeight; iy++)
         {
@@ -99,8 +108,7 @@ int main()
                 Y = Y0;
 
                 check_n = 0;
-                for (int i = 0; i < 4; i++) {check_N[i] = 0;}
-                for (int i = 0; i < 4; i++) {N[i] = 0;}
+                N = _mm_set1_ps(0);
                 int n = 0;
 
                 N = _mm_set1_ps(0);
@@ -127,25 +135,28 @@ int main()
                 // if (N != 255) printf("%d\n", N);
 
                 for (int i = 0; i < 4; i++) {arr_pixel[ix + i + iy * screenWidth] = {N[i] + 255, N[i] + 255, N[i] + 255, 255}; }
-                if (ix == 800 && iy == 450) arr_pixel[ix + iy * screenWidth] = RED;
+                if (ix == screenWidth / 2 && iy == screenHeight / 2) arr_pixel[ix + iy * screenWidth] = RED;
             }
         }
         
-        clock_t end = clock();
-        double time = (double) (end - start) / CLOCKS_PER_SEC;
-        int fps     = (int) 1 / time;
-        printf("time = %f\n", time);
-        printf("fps = %d\n", fps);
+        unsigned long long int end = __rdtsc();
+        counter++;
+        if ((100 < counter) && (counter < 1100)) fprintf(lab_file, "%ld\t", end - start);
+        if (counter < 1100) printf("%ld\n", counter);
+        if (counter >= 1100) printf("END!!!\n");
 
         BeginDrawing();
-            
+        #ifdef DRAW
             ClearBackground(RAYWHITE);
             My_texture = LoadTextureFromImage(My_image);
             DrawTexture(My_texture, 0, 0, WHITE);
-
+            DrawText(TextFormat("FPS: %i", GetFPS()), 10, 30, 20, LIME);
+        #endif
         EndDrawing();
     }
-
+    
+    fprintf(lab_file, "\n\n\n");
+    fclose(lab_file);
     CloseWindow();
     return 0;
 }
